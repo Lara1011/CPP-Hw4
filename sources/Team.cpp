6f9b546898a4225bb02a6e7bb9b3f722d78316a7
Lara Abu Hamad <laraabuhamad@gmail.com>
2023-05-17 22:57:01 +0300

    Updated Team

--- sources/Team.cpp
+++ sources/Team.cpp
@@ -16,25 +16,65 @@ namespace ariel{
 
     // Copy constructor
     Team::Team(const Team& other) {
+        leader = new Character(*other.leader); // Deep copy the leader
+
+        for (Character* member : other.members) {
+            members.push_back(new Character(*member)); // Deep copy each member
+        }
     }
 
 // Copy assignment operator
     Team& Team::operator=(const Team& other) noexcept{
+        if (this != &other) {
+            // Delete existing members
+            for (Character *member: members) {
+                delete member;
+            }
+            members.clear();
+
+            // Copy leader
+            delete leader;
+            leader = new Character(*other.leader); // Deep copy the leader
+
+            // Copy members
+            for (Character *member: other.members) {
+                members.push_back(new Character(*member)); // Deep copy each member
+            }
+        }
         return *this;
     }
 
     // Move constructor
     Team::Team(Team&& other)  noexcept {
+        leader = other.leader;
+        members = std::move(other.members);
+        other.leader = nullptr;
+        other.members.clear();
     }
 
     // Move assignment operator
     Team& Team::operator=(Team&& other)  noexcept {
+        if (this != &other) {
+// Delete existing members
+            for (Character *member: members) {
+                delete member;
+            }
+            members.clear();
+            // Move leader and members
+            delete leader;
+            leader = other.leader;
+            members = std::move(other.members);
+
+            // Reset other object
+            other.leader = nullptr;
+            other.members.clear();
+        }
         return *this;
     }
 
     Team::~Team() {
         for (Character* member : members) {
-            delete member;
+            delete &member;
         }
     }
 
